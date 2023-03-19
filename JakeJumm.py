cd57ba75571c58a99f7624c916d22383ac26d8cb
Bence <78733248+BXn4@users.noreply.github.com>
2023-03-19 10:55:21 +0100

    Update JakeJumm.py

--- JakeJumm.py
+++ JakeJumm.py
@@ -8,25 +8,31 @@ from curses import wrapper
 from curses.textpad import Textbox
 import shutil
 import string
+import sys
+import chardet
+
+fajlNeve = ""
+szoveg = []
 
 def main(stdscr):
+	global fajlNeve
 	space = " "
 	spacek = ""
-	fajlNeve = "ezegyfile1234567.py"
 	fajlKiterjesztes = ""
 	hosszuFajlNev = False
 	vanFajlKiterjesztes = False
-	try:
-		fajl = fajlNeve.rsplit('.', 1)
-		vanFajlKiterjesztes = True
-		fajlKiterjesztes = "." + fajl[1]
-		fajlNeve = fajl[0][:15]
-		if len(fajl[0]) > 15:
-			hosszuFajlNev = True
-	except:
-		if len(fajlNeve) > 15:
-			hosszuFajlNev = True
-		fajlNeve = fajlNeve[:15]
+	if fajlNeve:
+		try:
+			fajl = fajlNeve.rsplit('.', 1)
+			vanFajlKiterjesztes = True
+			fajlKiterjesztes = "." + fajl[1]
+			fajlNeve = fajl[0][:15]
+			if len(fajl[0]) > 15:
+				hosszuFajlNev = True
+		except:
+			if len(fajlNeve) > 15:
+				hosszuFajlNev = True
+			fajlNeve = fajlNeve[:15]
 	console_Y = shutil.get_terminal_size().lines
 	console_X = shutil.get_terminal_size().columns
 	console_C = int(console_X / 2)
@@ -45,8 +51,23 @@ def main(stdscr):
 		stdscr.addstr(console_Y - 1 , console_C, "{}..".format(fajlNeve) + "{}".format(fajlKiterjesztes), menubar)
 	else:
 		stdscr.addstr(console_Y - 1 , console_C, "{}".format(fajlNeve) + "{}".format(fajlKiterjesztes), menubar)
-	stdscr.addstr(0 , 0, "1 ")
+	for i, sorok in enumerate(szoveg):
+		stdscr.addstr(i , 0, "{} ".format(i))
+		stdscr.addstr(i, 2, sorok)
 	stdscr.refresh()
 	stdscr.getch()
 
+if len(sys.argv) < 2:
+    fajlNeve = "NewFile"
+else:
+    fajlNeve = sys.argv[1]
+    try:
+    	with open(fajlNeve, "rb") as olvas:
+    		szoveg = olvas.read(8192)
+    		eredmeny = chardet.detect(szoveg)
+    		kodolas = eredmeny['encoding']
+    	with open(fajlNeve, encoding=kodolas) as olvas:
+    		szoveg = [sorok.strip() for sorok in olvas]
+    except (FileNotFoundError):
+    	pass
 wrapper(main)
